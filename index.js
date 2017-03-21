c5b6f28f808247a0341c3ff3578d7a1856f6e480
fings <1059372332@qq.com>
2017-03-21 18:28:16 +0800

    creat repository

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+var another = require('./test');
+// console.log(another);
+
+var padkage = require('./package');
+console.log(padkage);
+
+
+// var test = require('lodash');
+// console.log(test);
---
+++ test.js
@@ -0,0 +1,12 @@
+
+
+
+
+module.exports.hello = 'world';
+
+// console.log(this);
+// console.log(exports.hello);
+// 
+// module.exports = {
+// 	a:'a'
+// }
