5e0a320528e52b51b6f054f7c6a5be16ceefa7a2
fings <1059372332@qq.com>
2017-03-22 12:57:42 +0800

    upgrade_project

---
+++ app/index.js
@@ -0,0 +1,38 @@
+
+
+// 主要核心逻辑入口
+// 
+// 
+// 
+// 
+
+
+const fs = require('fs');
+
+
+class App{
+	constructor(){
+
+	}
+	initServer(){
+		//init初始化
+		let _package = require('../package');//package is reserved word(X)
+
+		//高阶函数;
+		return (request,response)=>{
+			// process.cwd();//count work directory(node环境下运行)
+			fs.readFile('./public/index.html','utf8',(error,data)=>{	//异步API
+				// console.log(error);
+				// response.end(data);
+				response.end(JSON.stringify(_package));
+			})
+		}
+	}
+}
+// ES5
+// var App = function(){}
+// App.prototype.initServer = (request,response)=>{
+// 	response.end('hello fings');
+// 	}
+
+module.exports = App;
